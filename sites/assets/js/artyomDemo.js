64be98f2d7d3e021f2d15bdf36f9991066dcb6e5
Carlos Delgado <sdkcarlos@outlook.com>
2015-08-04 16:11:02 +0200

    Update

---
+++ sites/assets/js/artyomDemo.js
@@ -0,0 +1,28 @@
+
+
+/**
+ * Initialize artyom first.
+ * @param {type} param
+ */
+artyom.initialize({
+  lang : "en-GB",
+  continuous : true,
+  listen:true,
+  debug:true
+});
+
+/**
+ * When start listen and end listen do that !
+ * @param {type} param1
+ * @param {type} param2
+ */
+artyom.when("Recognition",function(e){
+    $("#artyom-status-label").css({
+      color:"green"
+    });
+});
+artyom.when("FinishRecognition",function(e){
+    $("#artyom-status-label").css({
+        color:"red"
+    });
+});
